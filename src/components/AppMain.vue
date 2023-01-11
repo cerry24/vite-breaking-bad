4deb479b22108bceb63a649df239d4d1b33fd177
cerry24 <samuelecerretti@gmail.com>
2023-01-11 16:42:52 +0100

    implement function getCards

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,6 +1,37 @@
 <script>
+import { store } from '../store.js';
+import axios from 'axios';
+
 export default {
-    name: 'AppMain'
+    name: 'AppMain',
+
+    data() {
+        return {
+            store,
+            apiUrl: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=10&offset=0'
+        }
+    },
+
+    methods: {
+        getCards() {
+            axios.get(this.apiUrl, {
+                params: {
+
+                }
+            })
+                .then((response) => {
+                    console.log(response.data.data);
+                    this.store.cardsList = response.data.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+        }
+    },
+
+    created() {
+        this.getCards()
+    },
 }
 </script>
 
