e2fadacded6c7638f489ef02a2c19d99c487b797
cerry24 <samuelecerretti@gmail.com>
2023-01-11 16:04:50 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+
+export default {
+  components: {
+    AppHeader,
+    AppMain
+  }
+}
+</script>
+
+<template>
+  <header>
+    <AppHeader />
+  </header>
+
+  <main>
+    <AppMain />
+  </main>
+</template>
+
+<style lang="scss">
+@use './styles/general.scss' as *;
+</style>
