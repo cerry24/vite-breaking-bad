1c011767a898b6b9a895241575de2fbaa7b40bc8
cerry24 <samuelecerretti@gmail.com>
2023-01-11 17:00:44 +0100

    implement header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,11 +5,24 @@ export default {
 </script>
 
 <template>
-    <div>
-
+    <div class="main-title">
+        <img src="http://vignette1.wikia.nocookie.net/yugiohenespanol/images/a/aa/Yugioh_anime_logo.jpg/revision/latest?cb=20130310042737&path-prefix=es"
+            alt="Yu-Gi-Oh logo">
+        <h1>YU-Gi-Oh Api</h1>
     </div>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
 
+.main-title {
+    display: flex;
+    background-color: $secondary-color;
+    padding: .5rem;
+
+    img {
+        width: 150px;
+        padding-right: 20px;
+    }
+}
 </style>
